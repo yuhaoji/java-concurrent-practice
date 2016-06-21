8d536db6298efe321fdbcd68e3813aa90bd45425
haoji.yu <mtworkwork@gmail.com>
2016-06-21 09:19:48 +0800

    添加信号量的例子

---
+++ src/main/java/org/mt/concurrent/practice/base/SemapDemo.java
@@ -0,0 +1,49 @@
+/* Project of UGC team
+
+======================
+Authors:haoji.yu
+
+======================
+Description:
+
+======================
+Major changs:
+
+add by haoji.yu 16/6/21
+
+*/
+package org.mt.concurrent.practice.base;
+
+import java.util.concurrent.ExecutorService;
+import java.util.concurrent.Executors;
+import java.util.concurrent.Semaphore;
+
+public class SemapDemo implements Runnable {
+
+    final Semaphore semaphore = new Semaphore(5);
+
+    public void run() {
+        try{
+            semaphore.acquire();
+            Thread.sleep(2000);
+            System.out.println(Thread.currentThread().getName()+" : is done!");
+            semaphore.release();
+        } catch (InterruptedException e) {
+            e.printStackTrace();
+        }
+    }
+
+    public static void main(String[] args) {
+        ExecutorService executorService = Executors.newFixedThreadPool(20);
+        final SemapDemo semapDemo = new SemapDemo();
+        for (int i = 0 ;i<20 ;i++) {
+            executorService.submit(semapDemo);
+        }
+//        executorService.shutdown();
+    }
+
+    /*
+       如果不加executorService.shutdown()的话,线程池会一直运行
+     */
+
+}
